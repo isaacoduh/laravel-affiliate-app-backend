7513b18bb4467d9a06ffcee72075ab85d7c71ace
Isaac Oduh <ufuomaoduh@gmail.com>
2021-11-30 18:41:29 +0100

    initial setup and register functionality

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create($request->only('first_name', 'last_name', 'email') + [
+            'password' => Hash::make($request->input('password')),
+            'is_admin' => 1
+        ]);
+        return response($user, Response::HTTP_CREATED);
+    }
+}
