d958fc6b3cc37ffeb4a5262645530365e338ee4e
Isaac Oduh <ufuomaoduh@gmail.com>
2021-11-30 23:39:52 +0100

    login, logout, authenticated user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,5 +19,35 @@ class AuthController extends Controller
             'is_admin' => 1
         ]);
         return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response([
+                'error' => 'invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+        $jwt = $user->createToken('token')->plainTextToken;
+        $cookie = cookie('jwt', $jwt, 60 * 24); // 24 hours
+        
+        return response([
+            'message' => 'Login Successful'
+        ])->withCookie($cookie);
+    }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+        return response([
+            'message' => 'logout success'
+        ])->withCookie($cookie);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class,'register']);
+Route::prefix('admin')->group(function(){
+    Route::post('/register', [AuthController::class,'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware(['auth:sanctum'])->group(function(){
+        Route::get('user',[AuthController::class, 'user']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
