be4cd65ddb46c91e09ce34e1257420d9a54fbe3f
Isaac Oduh <ufuomaoduh@gmail.com>
2021-12-01 14:17:37 +0100

    add affiliate scope and refactor auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\UpdateInfoRequest;
 use App\Http\Requests\UpdatePasswordRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,7 +19,7 @@ class AuthController extends Controller
     {
         $user = User::create($request->only('first_name', 'last_name', 'email') + [
             'password' => Hash::make($request->input('password')),
-            'is_admin' => 1
+            'is_admin' => $request->path() === 'api/admin/register' ? 1 : 0
         ]);
         return response($user, Response::HTTP_CREATED);
     }
@@ -32,7 +33,17 @@ class AuthController extends Controller
         }
 
         $user = Auth::user();
-        $jwt = $user->createToken('token',['admin'])->plainTextToken;
+        $adminLogin = $request->path() === 'api/admin/login';
+
+        if($adminLogin && !$user->is_admin) {
+            return response([
+                'error' => 'Access Denied!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $scope = $adminLogin ? 'admin' : 'affiliate';
+
+        $jwt = $user->createToken('token',[$scope])->plainTextToken;
+
         $cookie = cookie('jwt', $jwt, 60 * 24); // 24 hours
         
         return response([
@@ -42,7 +53,8 @@ class AuthController extends Controller
 
     public function user(Request $request)
     {
-        return $request->user();
+        $user =  $request->user();
+        return new UserResource($user);
     }
 
     public function logout()
---
+++ app/Http/Middleware/ScopeAffiliateMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ScopeAffiliateMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!$request->user()->tokenCan('affiliate')){
+            abort(401, 'Unauthorized');
+        }
+        return $next($request);
+    }
+}
