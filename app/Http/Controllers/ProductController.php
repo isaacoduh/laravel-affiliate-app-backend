121a64f92e2152c719e6877ae68244bac3e20b52
Isaac Oduh <ufuomaoduh@gmail.com>
2021-12-01 15:37:04 +0100

    product updated event and listener, add event to product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductUpdatedEvent;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -31,6 +32,7 @@ class ProductController extends Controller
         $product = Product::create($request->only('title','description','image','price'));
 
         // event - new product updated event
+        event(new ProductUpdatedEvent);
         return response($product, Response::HTTP_CREATED);
     }
 
@@ -57,6 +59,7 @@ class ProductController extends Controller
         $product->update($request->only('title', 'description', 'image', 'price'));
 
         // event - new product updated event
+        event(new ProductUpdatedEvent);
         return response($product, Response::HTTP_ACCEPTED);
     }
 
@@ -69,6 +72,7 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         $product->delete();
+        event(new ProductUpdatedEvent);
         return response(null, Response::HTTP_NO_CONTENT);
     }
 
