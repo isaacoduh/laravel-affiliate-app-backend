bc5dc712202e9653eb460cc11b57729389e8d51c
Isaac Oduh <ufuomaoduh@gmail.com>
2021-12-01 03:57:10 +0100

    admin link and link product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             AffiliateSeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            LinkSeeder::class
         ]);
     }
 }
