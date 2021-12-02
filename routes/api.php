c21150fd3e1af78baacadcd2c8fee9c99bab1816
Isaac Oduh <ufuomaoduh@gmail.com>
2021-12-02 06:03:47 +0100

    admin api tests complete

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\AffiliateController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LinkController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,16 +19,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::prefix('admin')->group(function(){
-    Route::post('/register', [AuthController::class,'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+// Route::prefix('admin')->group(function(){
+//     Route::post('/register', [AuthController::class,'register']);
+//     Route::post('/login', [AuthController::class, 'login']);
 
-    Route::middleware(['auth:sanctum','scope.admin'])->group(function(){
-        Route::get('user',[AuthController::class, 'user']);
+//     Route::middleware(['auth:sanctum','scope.admin'])->group(function(){
+//         Route::get('user',[AuthController::class, 'user']);
+//         Route::post('logout', [AuthController::class, 'logout']);
+//     });
+// });
+
+function common(string $scope)
+{
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware(['auth:sanctum', $scope])->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
         Route::post('logout', [AuthController::class, 'logout']);
+        Route::put('users/info', [AuthController::class, 'updateInfo']);
+        Route::put('users/password', [AuthController::class, 'updatePassword']);
+    });
+}
+
+Route::prefix('admin')->group(function() {
+    common('scope.admin');
+
+    Route::middleware(['auth:sanctum','scope.admin'])->group(function () {
+        Route::get('affiliates', [AffiliateController::class,'index']);
+        Route::get('users/{id}/links', [LinkController::class, 'index']);
+        Route::get('orders', [OrderController::class,'index']);
+
+        Route::apiResource('products', ProductController::class);
     });
 });
